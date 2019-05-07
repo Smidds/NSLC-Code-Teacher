8342394b188696d2bfc6ce4ce3563b5ffbf7047d
Smidds <isaachsmith.dev@gmail.com>
2019-05-07 16:00:12 -0500

    Quasar begin

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="hHh lpR fff">
+
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Arduino Teacher&#8482;
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
